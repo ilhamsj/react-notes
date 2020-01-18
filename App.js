797b02219bde53b1ac8773a7cd85fd9e5371f8b0
ilhamsj <ilhamsaputrajati@gmail.com>
2020-01-19 00:04:09 +0700

    nav stack error

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 export default class App extends Component {
   render() {
