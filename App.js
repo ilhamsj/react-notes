7af68a0ac53bfb78c31ac0dc1248baeda41fcb4e
ilhamsj <ilhamsaputrajati@gmail.com>
2020-01-19 00:19:40 +0700

    fix masked view

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
-export default class App extends Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+export default createAppContainer(AppNavigator);
